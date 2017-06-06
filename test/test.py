4b59136b0ac3f34e5e3c520f5be754d62f7a19c8
Andrey Kislyuk <kislyuk@gmail.com>
2017-06-06 16:20:39 -0700

    Begin hca-cli

---
+++ test/test.py
@@ -0,0 +1,12 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+from __future__ import absolute_import, division, print_function, unicode_literals
+
+import os, sys, unittest, tempfile, json, subprocess
+
+class TestHCACLI(unittest.TestCase):
+    pass
+
+if __name__ == '__main__':
+    unittest.main()
