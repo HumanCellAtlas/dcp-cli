2fd310f25cf1d4be2bb038108bb6e5d08652d336
Mackey22 <jjm447@cornell.edu>
2017-06-06 17:20:22 -0700

    Update test.py

--- test/test.py
+++ test/test.py
@@ -5,8 +5,14 @@ from __future__ import absolute_import, division, print_function, unicode_litera
 
 import os, sys, unittest, tempfile, json, subprocess
 
+pkg_root = os.path.abspath(os.path.join(os.path.dirname(__file__), '..'))
+sys.path.insert(0, pkg_root)
+
+import hca
+
 class TestHCACLI(unittest.TestCase):
-    pass
+    def test_hca_cli(self):
+        hca.main()
 
 if __name__ == '__main__':
     unittest.main()
