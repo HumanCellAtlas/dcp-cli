5a009dac97f4d75b7e5af0e22738107efdc0f5f0
James Mackey <jamesmackey@chanzuckerberg.com>
2017-06-06 18:11:43 -0700

    v0.0.6

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ from setuptools import setup, find_packages
 
 setup(
     name="hcacli",
-    version="0.0.5",
+    version="0.0.6",
     url='https://github.com/HumanCellAtlas/data-store-cli',
     license='Apache Software License',
     author='Human Cell Atlas contributors',
