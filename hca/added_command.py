78633d058777aaa65a56437bb5ff23c4bf84544f
Mackey22 <jjm447@cornell.edu>
2017-07-27 15:52:18 -0700

    Add AddedCommand superclass for all api endpoints. (#19)
    * Added encryption keys for travis.
    
    * Added region env variable to travis.
    
    * Created a new class AddedCommand that all api functions will subclass.
    
    * Added method stubs within AddedCommand.
    
    * Added method  with default functionality to add a cli parser based on the parameter data contained in the rest api defined endpoint. Higher level commands will need to override this method in order to be included in the cli.
    
    * Completed AddedCommand base class. Includes functionality to run from the cli, which packages up body payloads before sending them to the function that will be exposed to python users.
    
    * Functionality to send inputs to the api endpoint completed.
    
    * Fixed bugs in AddedCommand
    
    * Remove .travis.yml changes b/c they landed on a pr that's already been merged.
    
    * subclass object. inline help instead of setting pointless variable.

---
+++ hca/added_command.py
@@ -0,0 +1,274 @@
+import argparse
+import json
+
+import requests
+
+from constants import Constants
+
+
+class AddObject(argparse.Action):
+    """Object to parse json objects when they're inputted in the cli."""
+
+    def __init__(self, option_strings, dest, nargs=None, **kwargs):
+        super(AddObject, self).__init__(option_strings, dest, **kwargs)
+
+    def __call__(self, parser, namespace, values, option_string=None):
+        setattr(namespace, self.dest, json.loads(values))
+
+
+class AddedCommand(object):
+    """Class containing information to reach this endpoint."""
+
+    @classmethod
+    def _get_base_url(cls):
+        raise NotImplementedError("This function has not been implemented yet.")
+
+    @classmethod
+    def _get_endpoint_name(cls):
+        raise NotImplementedError("This function has not been implemented yet.")
+
+    @classmethod
+    def _get_endpoint_info(cls):
+        raise NotImplementedError("This function has not been implemented yet.")
+
+    @classmethod
+    def _get_arg_type(cls, arg_type_string):
+        argtype = None
+        if arg_type_string == "integer":
+            argtype = int
+        elif arg_type_string == "boolean":
+            argtype = bool
+        elif arg_type_string == "number":
+            argtype = float
+        return argtype
+
+    @classmethod
+    def _get_action(cls, arg_type_string):
+        if arg_type_string == "object":
+            return AddObject
+        return "store"
+
+    @classmethod
+    def _add_positional_args(cls, subparser):
+        endpoint_info = cls._get_endpoint_info()
+        for positional_arg in endpoint_info['positional']:
+            argtype = cls._get_arg_type(positional_arg['type'])
+
+            subparser.add_argument(
+                positional_arg['argument'],
+                nargs=None if positional_arg['required'] else "?",
+                help=endpoint_info['description'],
+                type=argtype
+            )
+
+    @classmethod
+    def _add_optional_args(cls, subparser):
+        endpoint_info = cls._get_endpoint_info()
+        for (optional_name, optional_data) in endpoint_info['options'].items():
+            argtype = cls._get_arg_type(optional_data['type'])
+            actiontype = cls._get_action(optional_data['type'])
+
+            optional_name_with_dashes = optional_name.replace("_", "-")
+            subparser.add_argument(
+                "--" + optional_name_with_dashes,
+                dest=optional_name,  # So we can send correctly formatted objects to api.
+                metavar=optional_data['metavar'],
+                required=optional_data['required'],
+                nargs="+" if optional_data['array'] else None,
+                help=endpoint_info['description'],
+                type=argtype,
+                action=actiontype
+            )
+
+    @classmethod
+    def add_parser(cls, subparsers):
+        """Add a command line subparser to handle this endpoint. Override this to add higher-level commands.
+
+        :param subparsers: The parent parser that this subparser will be added to.
+        """
+        endpoint_name = cls._get_endpoint_name()
+        endpoint_info = cls._get_endpoint_info()
+
+        subparser = subparsers.add_parser(endpoint_name, help=endpoint_info['description'])
+        cls._add_positional_args(subparser, endpoint_info)
+        cls._add_optional_args(subparser, endpoint_info)
+
+    @classmethod
+    def _get_ordered_path_args(cls, namespace):
+        """
+        Add positional arguments into the url.
+
+        :param namespace: The parsed arguments from argparse.
+        :return: The url to send requests to (minus query string).
+        """
+        endpoint_info = cls._get_endpoint_info()
+        all_positional_args_for_endpoint = [arg['argument'] for arg in endpoint_info['positional']]
+
+        given_positional_args = []
+        for positional_arg in all_positional_args_for_endpoint:
+            if positional_arg in namespace:
+                arg = namespace[positional_arg]
+                given_positional_args.append(arg)
+            else:
+                # No positional args can be skipped.
+                # If arg2 is present but arg1 isn't, arg2 can't take arg1's positional place.
+                break
+        return given_positional_args
+
+    @classmethod
+    def _add_arg(cls, arg, payload, hierarchy):
+        """
+        Add a given argument to its associated payload.
+
+        Arguments:
+         - arg: The value that is being assigned.
+         - payload: The payload structure before adding this value.
+         - hierarchy: List that describes where in payload to put arg (needed because of nested objects
+                      within a payload). Each element of hierarchy describes the data format of that layer.
+                      * If a level is an empty list, that means it's a list of the type taken from argpargs,
+                        so we just add each element as is to the payload.
+                      * If a level is a populated list, that means it's a list of objects with each element
+                        of that list corresponding to the name of the variable that the corresponding element
+                        in the inputs will be assigned to.
+                      * A level being a string (call it str) means str is a key to either the next level of the
+                        hierarchy (if there is one) or to the final argument that's being passed in here.
+        """
+        inner_payload = payload
+
+        # Iterating through payload structure to find the right value to set.
+        # Loop through hierarchy to define data structures that may not exist in payload yet.
+        for i in range(len(hierarchy) - 1):
+            curr_level = hierarchy[i]
+
+            if curr_level not in inner_payload:
+                inner_payload[curr_level] = [] if isinstance(hierarchy[i + 1], list) else {}
+            inner_payload = inner_payload[curr_level]
+
+        curr_level = hierarchy[-1]
+
+        # Command line argument represents a list of objects.
+        if isinstance(curr_level, list) and len(curr_level) > 0:
+            for string_object_values in arg:
+                object_values = string_object_values.split(Constants.OBJECT_SPLITTER)
+                if len(object_values) != len(curr_level):
+                    raise ValueError("Each argument must have a value. Use 'None' if you don't want to define it.")
+
+                obj = {}
+                for i, value in enumerate(object_values):
+                    if value == "None":
+                        continue
+                    argument_name = curr_level[i][0]
+                    argument_type = curr_level[i][1]
+                    if argument_type == "object":
+                        value = json.loads(value)
+                    elif argument_type == "integer":
+                        value = int(value)
+                    elif argument_type == "number":
+                        value = float(value)
+                    elif argument_type == "boolean" and value == "True":
+                        value = True
+                    elif argument_type == "boolean" and value == "False":
+                        value = False
+                    elif argument_type != "string":
+                        raise ValueError("Unknown type")
+                    obj[argument_name] = value
+                inner_payload.append(obj)
+
+        # Command line argument represents a list of standard types.
+        elif isinstance(curr_level, list):
+            for object_arg in arg:
+                inner_payload.append(object_arg)
+
+        # Command line argument represents a standard type.
+        else:
+            inner_payload[curr_level] = arg
+
+    @classmethod
+    def _build_body_payload(cls, namespace):
+        """
+        Build body for http requests.
+
+        Loop through all arguments provided from argparse and assign them to the body payload when appropriate.
+        :param namespace: The parsed arguments from argparse.
+        :return: Properly formatted body payload for http request.
+        """
+        endpoint_info = cls._get_endpoint_info()
+        body_payload = {}
+        for (arg_name, arg) in namespace.items():
+            # If the argument is positional, belongs in path, not a payload
+            if arg_name not in endpoint_info["options"]:
+                continue
+
+            payload_format = endpoint_info["options"][arg_name]["in"]
+            if payload_format == "body":
+                hierarchy = endpoint_info["options"][arg_name]["hierarchy"]
+                cls._add_arg(arg, body_payload, hierarchy)
+        return body_payload
+
+    @classmethod
+    def _build_non_body_payloads(cls, namespace):
+        """
+        Build query params and header params for http requests.
+
+        Loop through all arguments provided from argparse and assign them to their appropriate payload.
+        :param namespace: The parsed arguments from argparse.
+        :return: Properly formatted query params and header params for http request.
+        """
+        endpoint_info = cls._get_endpoint_info()
+        query_payload = {}
+        header_payload = {}
+        for (arg_name, arg) in namespace.items():
+            # If the argument is positional, belongs in path, not a payload
+            if arg_name not in endpoint_info["options"]:
+                continue
+
+            payload_format = endpoint_info["options"][arg_name]["in"]
+            hierarchy = endpoint_info["options"][arg_name]["hierarchy"]
+            if payload_format == "query":
+                cls._add_arg(arg, query_payload, hierarchy)
+            if payload_format == "header":
+                cls._add_arg(arg, header_payload, hierarchy)
+        return query_payload, header_payload
+
+    @classmethod
+    def run_cli(cls, namespace):
+        """Run this command using args from the cli. Override this to add higher-level commands."""
+        ordered_path_args = cls._get_ordered_path_args(namespace)
+        body_payload = cls._build_body_payload(namespace)
+        return cls.run(*ordered_path_args, body=body_payload, **namespace)
+
+    @classmethod
+    def run(cls, *args, **kwargs):
+        """Function that will be exposed to the api users."""
+        endpoint_name = cls._get_endpoint_name()
+        split_endpoint = endpoint_name.split("-")[1]
+
+        query_route = [split_endpoint]
+        query_route.extend(args)
+        url = cls._get_base_url() + "/" + "/".join(query_route)
+        query_payload, header_payload = cls._build_non_body_payloads(kwargs)
+
+        param_methods = ["get", "options", "head", "delete"]
+        json_methods = ["post", "put", "patch"]
+        method = endpoint_name[:endpoint_name.find("-")]
+
+        if method in param_methods:
+            request = requests.request(
+                method,
+                url,
+                params=query_payload,
+                headers=header_payload,
+                stream=kwargs.get('stream', False)
+            )
+        elif method in json_methods:
+            request = requests.request(
+                method,
+                url,
+                json=kwargs.get('body', None),
+                params=query_payload,
+                headers=header_payload,
+                stream=kwargs.get('stream', False)
+            )
+        else:
+            raise ValueError("Bad request type")
+        return request
