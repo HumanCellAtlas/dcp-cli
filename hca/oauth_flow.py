dc0eff51c729eca93d30d09b0cfcf13e44dec685
Mackey22 <jjm447@cornell.edu>
2017-08-04 16:27:52 -0700

    Use GOOGLE_APPLICATION_CREDENTIALS env var instead of gsutil. rm tty popup for testing.

--- hca/oauth_flow.py
+++ hca/oauth_flow.py
@@ -63,24 +63,24 @@ def get_access_token(path_to_credentials_file, client_secrets_directory, scope):
     credentials = storage.get()
 
     if not credentials:
-        if sys.stdin.isatty():
-            # Set up a Flow object that will bring people to a browser to authenticate.
-            flow = client.flow_from_clientsecrets(client_secrets,
-                                                  scope=scope,
-                                                  message=tools.message_if_missing(client_secrets))
-            credentials = tools.run_flow(flow, storage, flags)
-        else:
-            try:
-                credentials = client.GoogleCredentials.get_application_default()
-                credentials.scopes = set([scope])
+        # if sys.stdin.isatty():
+        #     # Set up a Flow object that will bring people to a browser to authenticate.
+        #     flow = client.flow_from_clientsecrets(client_secrets,
+        #                                           scope=scope,
+        #                                           message=tools.message_if_missing(client_secrets))
+        #     credentials = tools.run_flow(flow, storage, flags)
+        # else:
+        #     try:
+        credentials = client.GoogleCredentials.get_application_default()
+        credentials.scopes = set([scope])
 
-                storage.put(credentials)
-                credentials.set_store(storage)
+        storage.put(credentials)
+        credentials.set_store(storage)
 
-            except client.ApplicationDefaultCredentialsError:
-                raise client.ApplicationDefaultCredentialsError(
-                    "Store JSON-formatted oauth2client.client.OAuth2Credentials in {}!"
-                    .format(path_to_credentials_file)
-                )
+        # except client.ApplicationDefaultCredentialsError:
+        #     raise client.ApplicationDefaultCredentialsError(
+        #         "Store JSON-formatted oauth2client.client.OAuth2Credentials in {}."
+        #         .format(path_to_credentials_file)
+        #     )
 
     return credentials.get_access_token()
