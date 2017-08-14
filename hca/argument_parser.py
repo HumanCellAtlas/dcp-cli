374297da432a6e83923b63904dd1f10177ce815e
Mackey22 <jjm447@cornell.edu>
2017-08-13 23:01:44 -0700

    NonPrintingParser raises PrintingExceptionn instead of printing

---
+++ hca/argument_parser.py
@@ -0,0 +1,15 @@
+import argparse
+
+from .error import PrintingException
+
+class NonPrintingParser(argparse.ArgumentParser):
+    """ArgumentParser that will return help and error strings instead of printing."""
+
+    def __init__(self, *args, **kwargs):
+        super(NonPrintingParser, self).__init__(*args, **kwargs)
+
+    def print_usage(self, file=None):
+        raise PrintingException(self.format_usage())
+
+    def print_help(self, file=None):
+        raise PrintingException(self.format_help())
