4339ae1d171100a988992a3820cbb58b1541cfdc
Mackey22 <jjm447@cornell.edu>
2017-08-14 09:36:10 -0700

    Don't override init. Better comments.

--- hca/argument_parser.py
+++ hca/argument_parser.py
@@ -6,9 +6,6 @@ from .error import PrintingException
 class NonPrintingParser(argparse.ArgumentParser):
     """ArgumentParser that will return help and error strings instead of printing."""
 
-    def __init__(self, *args, **kwargs):
-        super(NonPrintingParser, self).__init__(*args, **kwargs)
-
     def print_usage(self, file=None):
         raise PrintingException(self.format_usage())
 
