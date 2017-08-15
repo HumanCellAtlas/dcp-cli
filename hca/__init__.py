554ca711ecc8f98184985200f85b85980e598607
Mackey22 <jjm447@cornell.edu>
2017-08-15 12:32:39 -0700

    Only check system platform once - not in each iteration

--- hca/__init__.py
+++ hca/__init__.py
@@ -16,8 +16,13 @@ def main():
     """Entrance to functionality."""
     from .cli import CLI
     cli = CLI()
-    response = cli.make_request(sys.argv[1:])
 
+    # Windows includes carriage returns
+    if sys.platform == 'win32':
+        import msvcrt
+        msvcrt.setmode(sys.stdout.fileno(), os.O_BINARY)
+
+    response = cli.make_request(sys.argv[1:])
     if isinstance(response, requests.Response):
         for chunk in response.iter_content(chunk_size=Constants.CHUNK_SIZE, decode_unicode=True):
             if chunk:  # filter out keep-alive new chunks
@@ -34,9 +39,4 @@ def _write_binary_to_stdout(content):
     if six.PY3:
         sys.stdout.buffer.write(content)
     else:
-        # Windows includes carriage returns
-        if sys.platform == 'win32':
-            import msvcrt
-            msvcrt.setmode(sys.stdout.fileno(), os.O_BINARY)
-
         sys.stdout.write(content)
