37b358f71029f2fb2b8532bec9a50ef6b30c07c2
Sam Pierson <sam@sampierson.com>
2017-09-27 16:21:39 -0700

    Use "hca dss <command>" to invoke DSS commands

---
+++ hca/cli.py
@@ -0,0 +1,51 @@
+# coding: utf-8
+
+"""
+Human Cell Atlas Command Line Interface
+"""
+
+import argparse, sys, os
+
+import hca.dss.cli
+
+
+class CLI:
+
+    def __init__(self):
+        # Windows includes carriage returns
+        if sys.platform == 'win32':
+            import msvcrt
+            msvcrt.setmode(sys.stdout.fileno(), os.O_BINARY)
+
+        self._setup_arg_parser()
+
+    def run(self, command_line_args=None):
+        argparser_args = self.top_parser.parse_args(command_line_args or sys.argv[1:])
+        if not argparser_args.__contains__('func'):
+            self.top_parser.print_help()
+            sys.exit(1)
+        command_function = argparser_args.func
+        del argparser_args.func
+        command_function(argparser_args)
+
+    def _setup_arg_parser(self):
+        self.top_parser = argparse.ArgumentParser(description=__doc__,
+                                                  formatter_class=argparse.RawDescriptionHelpFormatter)
+        top_subparsers = self.top_parser.add_subparsers(title="Commands",
+                                                        help="Use \"hca <command> -h\" to get detailed command help.\"")
+
+        help_parser = top_subparsers.add_parser('help', add_help=False)
+        help_parser.set_defaults(func=self._help)
+
+        hca.dss.cli.add_commands(top_subparsers)
+
+    def _help(self, args):
+        self.top_parser.print_usage()
+        print("""
+Commands:
+
+    help    print this message
+    dss     manipulate HCA Data Store
+
+Use "hca <command> -h" to get detailed command help.
+        """)
--- test/__init__.py
+++ test/__init__.py
@@ -1,1 +1,21 @@
+import io, sys
 
+import six
+
+
+class CapturingIO:
+    def __init__(self, stream_name='stdout'):
+        self.stream_name = stream_name
+        self.buffer = six.StringIO()
+        self.orig_stream = getattr(sys, stream_name)
+
+    def __enter__(self):
+        setattr(sys, self.stream_name, self.buffer)
+        return self
+
+    def __exit__(self, *args):
+        setattr(sys, self.stream_name, self.orig_stream)
+
+    def captured(self):
+        self.buffer.seek(0, io.SEEK_SET)
+        return self.buffer.read()
