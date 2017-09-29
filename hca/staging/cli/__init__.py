5bcb85408f55995959ad2faaa6d1f8ff370270df
Sam Pierson <sam@sampierson.com>
2017-09-28 22:04:09 -0700

    Command: "hca staging help"

--- hca/staging/cli/__init__.py
+++ hca/staging/cli/__init__.py
@@ -6,5 +6,21 @@ def add_commands(subparsers):
     staging_parser = subparsers.add_parser('staging')
     staging_subparsers = staging_parser.add_subparsers()
 
+    help_parser = staging_subparsers.add_parser('help',
+                                                description="Display list of staging commands.")
+    help_parser.set_defaults(func=_help)
+
     SelectCommand.add_parser(staging_subparsers)
     ListAreasCommand.add_parser(staging_subparsers)
+
+
+def _help(args):
+    print("""
+hca staging commands:
+
+    help     print this message
+    select   select a staging area to use
+    areas    list staging areas we know about
+
+Use "hca staging <command> -h" to get detailed command help.
+""")
