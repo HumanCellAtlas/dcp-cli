cf18405cc4c1949ece0fa2b69080e473075ac96b
Tony Tung <tonytung@merly.org>
2017-10-02 10:31:41 -0700

    regenerate bindings. (#62)

--- hca/dss/autogenerated/head_files.py
+++ hca/dss/autogenerated/head_files.py
@@ -7,8 +7,8 @@ class HeadFiles(AddedCommand):
 
     @classmethod
     def _get_base_url(cls):
-        return "https://dss.staging.data.humancellatlas.org/v1"
+        return "https://dss.dev.data.humancellatlas.org/v1"
 
     @classmethod
     def _get_endpoint_info(cls):
-        return {u'description': u"Given a file UUID, return the metadata for the latest version of that file.  If the version is provided, that\nversion's metadata is returned instead.  The metadata is returned in the headers.\n", u'body_params': {}, u'positional': [{u'description': u'A RFC4122-compliant ID for the file.', u'format': None, u'pattern': None, u'required': True, u'argument': u'uuid', u'required_for': [u'/files/{uuid}'], u'type': u'string'}], u'seen': False, u'requires_auth': False, u'options': {u'version': {u'hierarchy': [u'version'], u'in': u'query', u'description': u'Timestamp of file creation in RFC3339.  If this is not provided, the latest version is returned.', u'required_for': [], u'format': u'date-time', u'pattern': None, u'array': False, u'required': False, u'type': u'string', u'metavar': None}}}
+        return {u'description': u"Given a file UUID, return the metadata for the latest version of that file.  If the version is provided, that\nversion's metadata is returned instead.  The metadata is returned in the headers.\n", u'body_params': {}, u'positional': [{u'description': u'A RFC4122-compliant ID for the file.', u'format': None, u'pattern': None, u'required': True, u'argument': u'uuid', u'required_for': [u'/files/{uuid}'], u'type': u'string'}], u'seen': False, u'requires_auth': False, u'options': {u'replica': {u'hierarchy': [u'replica'], u'in': u'query', u'description': u'Replica to fetch from.', u'required_for': [u'/files/{uuid}'], u'format': None, u'pattern': None, u'array': False, u'required': True, u'type': u'string', u'metavar': None}, u'version': {u'hierarchy': [u'version'], u'in': u'query', u'description': u'Timestamp of file creation in RFC3339.  If this is not provided, the latest version is returned.', u'required_for': [], u'format': u'date-time', u'pattern': None, u'array': False, u'required': False, u'type': u'string', u'metavar': None}}}
