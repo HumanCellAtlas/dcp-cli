ac2a757208a3cb5a7fa5b8d16f8f42be2b8910d6
Sam Pierson <sam@sampierson.com>
2017-10-30 15:37:29 -0700

    Upload: factor common test helpers out of test_upload_file.py

--- test/upload/__init__.py
+++ test/upload/__init__.py
@@ -1,4 +1,40 @@
+import base64
+import json
 import os
+import uuid
+
+import tweak
+
+import hca
+from hca.upload import UploadArea, UploadAreaURN
 
 UPLOAD_BUCKET_NAME_TEMPLATE = 'bogo-bucket-{deployment_stage}'
 TEST_UPLOAD_BUCKET = UPLOAD_BUCKET_NAME_TEMPLATE.format(deployment_stage=os.environ['DEPLOYMENT_STAGE'])
+
+
+def setup_tweak_config():
+    config = tweak.Config(hca.TWEAK_PROJECT_NAME)
+    config.upload = {
+        'areas': {},
+        'bucket_name_template': UPLOAD_BUCKET_NAME_TEMPLATE
+    }
+    config.save()
+
+
+def mock_current_upload_area():
+    area = mock_upload_area()
+    area.select()
+    return area
+
+
+def mock_upload_area():
+    """
+    Create a UUID and URN for a fake upload area, store it in Tweak config.
+    """
+    stage = os.environ['DEPLOYMENT_STAGE']
+    area_uuid = str(uuid.uuid4())
+    creds = {'AWS_ACCESS_KEY_ID': 'foo', 'AWS_SECRET_ACCESS_KEY': 'bar'}
+    encoded_creds = base64.b64encode(json.dumps(creds).encode('ascii')).decode('ascii')
+    urn = "dcp:upl:aws:{}:{}:{}".format(stage, area_uuid, encoded_creds)
+    area = UploadArea(urn=UploadAreaURN(urn))
+    return area
--- test/upload/python_bindings/test_upload_file.py
+++ test/upload/python_bindings/test_upload_file.py
@@ -14,44 +14,23 @@ from ... import reset_tweak_changes
 pkg_root = os.path.abspath(os.path.join(os.path.dirname(__file__), '..', '..'))  # noqa
 sys.path.insert(0, pkg_root)  # noqa
 
-import hca
 from hca import upload
-from hca.upload.upload_area import UploadArea
-from hca.upload.upload_area_urn import UploadAreaURN
-from .. import UPLOAD_BUCKET_NAME_TEMPLATE, TEST_UPLOAD_BUCKET
+from .. import TEST_UPLOAD_BUCKET, mock_current_upload_area, setup_tweak_config
 
 
 class TestUploadFileUpload(unittest.TestCase):
 
-    def setUp(self):
-        self.stage = 'test'
-        self.area_uuid = str(uuid.uuid4())
-        creds = {'AWS_ACCESS_KEY_ID': 'foo', 'AWS_SECRET_ACCESS_KEY': 'bar'}
-        encoded_creds = base64.b64encode(json.dumps(creds).encode('ascii')).decode('ascii')
-        self.urn = "dcp:upl:aws:{}:{}:{}".format(self.stage, self.area_uuid, encoded_creds)
-        self.area = UploadArea(urn=UploadAreaURN(self.urn))
-        self.area.select()
-
-    def setup_tweak_config(self):
-        config = tweak.Config(hca.TWEAK_PROJECT_NAME)
-        config.upload = {
-            'areas': {self.area_uuid: self.urn},
-            'current_area': self.area_uuid,
-            'bucket_name_template': UPLOAD_BUCKET_NAME_TEMPLATE
-        }
-        config.save()
-
     @mock_s3
     @reset_tweak_changes
     def test_file_upload(self):
-        self.setup_tweak_config()
+        setup_tweak_config()
+        area = mock_current_upload_area()
         s3 = boto3.resource('s3')
         s3.Bucket(TEST_UPLOAD_BUCKET).create()
-        area = UploadArea(uuid=self.area_uuid)
 
         upload.upload_file('LICENSE')
 
-        obj = s3.Bucket(TEST_UPLOAD_BUCKET).Object("{}/LICENSE".format(self.area_uuid))
+        obj = s3.Bucket(TEST_UPLOAD_BUCKET).Object("{}/LICENSE".format(area.uuid))
         with open('LICENSE', 'rb') as fh:
             expected_contents = fh.read()
             self.assertEqual(obj.get()['Body'].read(), expected_contents)
@@ -59,14 +38,14 @@ class TestUploadFileUpload(unittest.TestCase):
     @mock_s3
     @reset_tweak_changes
     def test_file_upload_with_target_filename_option(self):
-        self.setup_tweak_config()
+        setup_tweak_config()
+        area = mock_current_upload_area()
         s3 = boto3.resource('s3')
         s3.Bucket(TEST_UPLOAD_BUCKET).create()
-        area = UploadArea(uuid=self.area_uuid)
 
         upload.upload_file('LICENSE', target_filename='POO')
 
-        obj = s3.Bucket(TEST_UPLOAD_BUCKET).Object("{}/POO".format(self.area_uuid))
+        obj = s3.Bucket(TEST_UPLOAD_BUCKET).Object("{}/POO".format(area.uuid))
         with open('LICENSE', 'rb') as fh:
             expected_contents = fh.read()
             self.assertEqual(obj.get()['Body'].read(), expected_contents)
