94315d61461ccad8f03b044aa00c482174b99e97
Andrey Kislyuk <kislyuk@gmail.com>
2017-11-30 10:57:30 -0800

    Minor changes to Sphinx autodoc config

--- docs/conf.py
+++ docs/conf.py
@@ -19,8 +19,11 @@
 #
 import os
 import sys
-sys.path.insert(0, os.path.abspath('..'))
 
+sys.path.insert(0, os.path.abspath('..'))  # noqa
+
+from hca.dss import DSSClient
+dss_client = DSSClient()
 
 # -- General configuration ------------------------------------------------
 
