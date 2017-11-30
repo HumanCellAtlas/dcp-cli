108f97bd5166f709831f95a4815fcfc0412bf24d
Andrey Kislyuk <kislyuk@gmail.com>
2017-11-30 10:56:49 -0800

    Add new in-memory codegen Swagger CLI

--- test/test_dss_api.py
+++ test/test_dss_api.py
@@ -1,144 +1,131 @@
 #!/usr/bin/env python
 # coding: utf-8
 
-import unittest, os, sys, filecmp, shutil, tempfile, uuid
-
-import requests
+import unittest, os, sys, filecmp, uuid, tempfile
 
 pkg_root = os.path.abspath(os.path.join(os.path.dirname(__file__), '..'))  # noqa
 sys.path.insert(0, pkg_root)  # noqa
 
 import hca.dss
-from . import reset_tweak_changes
+from hca.util.compat import USING_PYTHON2
+from test import reset_tweak_changes
 
+if USING_PYTHON2:
+    import backports.tempfile
+    TemporaryDirectory = backports.tempfile.TemporaryDirectory
+else:
+    TemporaryDirectory = tempfile.TemporaryDirectory
 
 class TestDssApi(unittest.TestCase):
+    staging_bucket = "org-humancellatlas-dss-cli-test"
 
     def test_python_upload_download(self):
-        dirpath = os.path.dirname(os.path.realpath(__file__))
-        bundle_path = os.path.join(dirpath, "bundle")
+        with TemporaryDirectory() as dest_dir:
+            dirpath = os.path.dirname(os.path.realpath(__file__))
+            bundle_path = os.path.join(dirpath, "bundle")
 
-        namespace = {'file_or_dir': [bundle_path],
-                     'replica': "aws",
-                     'staging_bucket': "org-humancellatlas-dss-cli-test"}
+            client = hca.dss.DSSClient()
 
-        bundle_output = hca.dss.upload(**namespace)
+            bundle_output = client.upload(src_dir=bundle_path, replica="aws", staging_bucket=self.staging_bucket)
 
-        downloaded_path = os.path.join(dirpath, "TestDownload")
-        hca.dss.download(bundle_output['bundle_uuid'], name=downloaded_path)
-        for file_info in hca.dss.Download._download_bundle(dict(
-                uuid=bundle_output['bundle_uuid'],
-                replica="aws"))[0]:
-            self.assertNotEqual(file_info['content-type'], "binary/octet-stream")
+            client.download(bundle_uuid=bundle_output['bundle_uuid'], dest_name=dest_dir, replica="aws")
 
-        # Check that contents are the same
-        for file in os.listdir(bundle_path):
-            uploaded_file = os.path.join(bundle_path, file)
-            downloaded_file = os.path.join(downloaded_path, file)
-            self.assertTrue(filecmp.cmp(uploaded_file, downloaded_file, False))
-
-        if os.path.exists(downloaded_path):
-            shutil.rmtree(downloaded_path)
+            # Check that contents are the same
+            for file in os.listdir(bundle_path):
+                uploaded_file = os.path.join(bundle_path, file)
+                downloaded_file = os.path.join(dest_dir, file)
+                self.assertTrue(filecmp.cmp(uploaded_file, downloaded_file, False))
 
     def test_python_upload_lg_file(self):
-        with tempfile.NamedTemporaryFile(suffix=".bin") as fh:
-            fh.write(os.urandom(64 * 1024 * 1024 + 1))
-            fh.flush()
+        with TemporaryDirectory() as src_dir, TemporaryDirectory() as dest_dir:
+            with tempfile.NamedTemporaryFile(dir=src_dir, suffix=".bin") as fh:
+                fh.write(os.urandom(64 * 1024 * 1024 + 1))
+                fh.flush()
 
-            namespace = {'file_or_dir': [fh.name],
-                         'replica': "aws",
-                         'staging_bucket': "org-humancellatlas-dss-cli-test"}
+                client = hca.dss.DSSClient()
+                bundle_output = client.upload(src_dir=src_dir, replica="aws", staging_bucket=self.staging_bucket)
 
-            bundle_output = hca.dss.upload(**namespace)
+                client.download(bundle_output['bundle_uuid'], replica="aws", dest_name=dest_dir)
 
-            tempdir = tempfile.mkdtemp()
-            try:
-                hca.dss.download(bundle_output['bundle_uuid'], name=tempdir)
-
-                downloaded_file = os.path.join(tempdir, os.path.basename(fh.name))
+                downloaded_file = os.path.join(dest_dir, os.path.basename(fh.name))
                 self.assertTrue(filecmp.cmp(fh.name, downloaded_file, False))
-            finally:
-                if os.path.exists(tempdir):
-                    shutil.rmtree(tempdir)
 
     def test_python_bindings(self):
-        dirpath = os.path.dirname(os.path.realpath(__file__))
-        bundle_path = os.path.join(dirpath, "bundle")
-        staging_bucket = "org-humancellatlas-dss-cli-test"
-        namespace = {'file_or_dir': [bundle_path],
-                     'replica': "aws",
-                     'staging_bucket': "org-humancellatlas-dss-cli-test"}
+        bundle_path = os.path.join(os.path.dirname(os.path.realpath(__file__)), "bundle")
 
-        bundle_output = hca.dss.upload(**namespace)
+        client = hca.dss.DSSClient()
+        bundle_output = client.upload(src_dir=bundle_path, replica="aws", staging_bucket=self.staging_bucket)
+        bundle_uuid = bundle_output['bundle_uuid']
 
-        hca.dss.download(bundle_output['bundle_uuid'], name=os.path.join(dirpath, "TestDownload"))
+        with TemporaryDirectory() as dest_dir:
+            client.download(bundle_uuid=bundle_output['bundle_uuid'], replica="aws", dest_name=dest_dir)
 
         # Test get-files and head-files
         file_ = bundle_output['files'][0]
-        self.assertTrue(hca.dss.get_files(file_['uuid'], replica="aws").ok)
-        self.assertTrue(hca.dss.head_files(file_['uuid'], replica="aws").ok)
+        with client.get_file.stream(uuid=file_['uuid'], replica="aws") as fh:
+            while True:
+                chunk = fh.raw.read(1024)
+                if chunk == b"":
+                    break
+        self.assertTrue(client.head_file(uuid=file_['uuid'], replica="aws").ok)
 
         # Test get-bundles
-        bundle_uuid = bundle_output['bundle_uuid']
-        self.assertTrue(hca.dss.get_bundles(bundle_uuid, replica="aws").ok)
+        res = client.get_bundle(uuid=bundle_uuid, replica="aws")
+        self.assertEqual(res["bundle"]["uuid"], bundle_uuid)
 
         # Test put-files
         file_uuid = str(uuid.uuid4())
         bundle_uuid = str(uuid.uuid4())
-        source_url = "s3://{}/{}/{}".format(staging_bucket, file_['uuid'], file_['name'])
-        self.assertTrue(hca.dss.put_files(file_uuid, creator_uid=1, bundle_uuid=bundle_uuid, source_url=source_url).ok)
+        source_url = "s3://{}/{}/{}".format(self.staging_bucket, file_['uuid'], file_['name'])
+        client.put_file(uuid=file_uuid, creator_uid=1, bundle_uuid=bundle_uuid, source_url=source_url)
 
         # Test put-bundles
         files = [{'indexed': True,
                   'name': file_['name'],
                   'uuid': file_['uuid'],
                   'version': file_['version']}]
-        resp = hca.dss.put_bundles(bundle_uuid, files=files, creator_uid=1, replica="aws")
-        self.assertTrue(resp.ok)
+        res = client.put_bundle(uuid=bundle_uuid, files=files, creator_uid=1, replica="aws")
+        self.assertTrue(len(res["version"]) > 0)
 
-    def test_python_api_url(self):
-        kwargs = {'uuid': "fake_uuid",
-                  'replica': "aws",
-                  'api_url': "https://thisisafakeurljslfjlshsfs.com"}
-        self.assertRaises(requests.exceptions.ConnectionError,
-                          hca.dss.get_files,
-                          **kwargs)
+        with self.assertRaisesRegexp(Exception, "Missing query parameter 'replica'"):
+            res = client.put_bundle(uuid=bundle_uuid, files=[], creator_uid=1)
 
     def test_python_subscriptions(self):
+        client = hca.dss.DSSClient()
+
         query = {'bool': {}}
-        resp = hca.dss.put_subscriptions(es_query=query, callback_url="www.example.com", replica="aws")
-        subscription_uuid = resp.json()['uuid']
+        resp = client.put_subscription(es_query=query, callback_url="www.example.com", replica="aws")
+        subscription_uuid = resp['uuid']
 
-        self.assertEqual(201, resp.status_code)
-        self.assertIn('uuid', resp.json())
+        resp = client.get_subscriptions(replica="aws")
+        self.assertTrue(subscription_uuid in [s['uuid'] for s in resp['subscriptions']])
 
-        resp = hca.dss.get_subscriptions("aws")
-        self.assertEqual(200, resp.status_code)
-        self.assertTrue(subscription_uuid in [s['uuid'] for s in resp.json()['subscriptions']])
+        resp = client.get_subscription(replica="aws", uuid=subscription_uuid)
+        self.assertEqual(subscription_uuid, resp['uuid'])
 
-        resp = hca.dss.get_subscriptions(replica="aws", uuid=subscription_uuid)
-        self.assertEqual(200, resp.status_code)
-        self.assertEqual(subscription_uuid, resp.json()['uuid'])
+        resp = client.delete_subscription(uuid=subscription_uuid, replica="aws")
+        self.assertIn('timeDeleted', resp)
 
-        resp = hca.dss.delete_subscriptions(uuid=subscription_uuid, replica="aws")
-        self.assertEqual(200, resp.status_code)
-        self.assertIn('timeDeleted', resp.json())
-
-        resp = hca.dss.get_subscriptions("aws", uuid=subscription_uuid)
-        self.assertEqual(404, resp.status_code)
+        with self.assertRaisesRegexp(Exception, "Cannot find subscription!"):
+            resp = client.get_subscription(replica="aws", uuid=subscription_uuid)
 
     @reset_tweak_changes
-    def test_python_login(self):
-        from tweak import Config
+    def test_python_login_logout(self):
+        client = hca.dss.DSSClient()
+        query = {'bool': {}}
+        resp = client.put_subscription(es_query=query, callback_url="www.example.com", replica="aws")
+        self.assertIn("uuid", resp)
 
         access_token = "test_access_token"
-        out = {'completed': True}
 
-        login = hca.dss.login(access_token=access_token)
-        config = Config(hca.TWEAK_PROJECT_NAME)
+        client.login(access_token=access_token)
+        config = hca.get_config()
 
-        self.assertEqual(login, out)
-        self.assertEqual(config.login.access_token, access_token)
+        self.assertEqual(config.oauth2_token.access_token, access_token)
+
+        client.logout()
+
+        self.assertNotIn("oauth2_token", config)
 
 
 if __name__ == '__main__':
--- test/test_dss_cli.py
+++ test/test_dss_cli.py
@@ -6,313 +6,62 @@ from __future__ import absolute_import, division, print_function, unicode_litera
 import os
 import sys
 import unittest
-from io import open
-import subprocess
+import json
 
-import six
+pkg_root = os.path.abspath(os.path.join(os.path.dirname(__file__), '..'))  # noqa
+sys.path.insert(0, pkg_root)  # noqa
 
-pkg_root = os.path.abspath(os.path.join(os.path.dirname(__file__), '..'))
-sys.path.insert(0, pkg_root)
-
+import hca
+import hca.cli
 import hca.dss
-import hca.dss.cli
-import hca.dss.regenerate_api
-import hca.dss.constants
-from hca.cli import CLI
-from . import CapturingIO, reset_tweak_changes
+
+from test import CapturingIO, reset_tweak_changes
 
 
 class TestDssCLI(unittest.TestCase):
-    """Test the entire module."""
-
-    def test_make_name(self):
-        """Test make_name in parser.py."""
-        http_method = "get"
-        path_split = ["bundles"]
-        self.assertEqual(
-            hca.dss.regenerate_api._make_name(http_method, path_split),
-            "get-bundles"
-        )
-
-        path_split.append("cmd")
-        self.assertEqual(
-            hca.dss.regenerate_api._make_name(http_method, path_split),
-            "get-bundles-cmd"
-        )
-
-    def test_index_parameters(self):
-        """Test index_parameters in parser.py."""
-        self.maxDiff = None
-
-        params = {
-            "description": "Returns a list of bundles matching given criteria.\n",
-            "responses": {
-              "200": {
-                "description": "OK",
-                "schema": {
-                  "properties": {
-                    "bundles": {
-                      "items": {
-                        "$ref": "#/definitions/Bundle"
-                      },
-                      "type": "array"
-                    }
-                  },
-                  "required": [
-                    "bundles"
-                  ],
-                  "type": "object"
-                }
-              }
-            },
-            "summary": "Query bundles"
-        }
-        self.assertEqual(hca.dss.regenerate_api.index_parameters(None, params), {})
-
-        params["parameters"] = [
-            {
-                "description": "Bundle unique ID.",
-                "in": "path",
-                "name": "uuid",
-                "required": True,
-                "type": "string",
-            }
-        ]
-        self.assertEqual(
-            hca.dss.regenerate_api.index_parameters(None, params),
-            {"uuid": {
-                "description": "Bundle unique ID.",
-                "in": "path",
-                "name": "uuid",
-                "required": True,
-                "type": "string",
-                "array": False,
-                'req': True,
-                'hierarchy': ['uuid']
-            }})
-
-        params['parameters'] = [{
-            "in": "body",
-            "name": "extras",
-            "required": True,
-            "schema": {
-              "properties": {
-                "bundle_uuid": {
-                  "description": "A RFC4122-compliant ID.",
-                  "type": "string"
-                },
-                "timestamp": {
-                  "description": "Timestamp of file creation in RFC3339.",
-                  "format": "date-time",
-                  "type": "string"
-                }
-              },
-              "required": [
-                "bundle_uuid",
-              ],
-              "type": "object"
-            }
-          }
-        ]
-        self.assertEqual(
-            hca.dss.regenerate_api.index_parameters(None, params),
-            {"timestamp": {
-                "description": "Timestamp of file creation in RFC3339.",
-                "in": "body",
-                "name": "timestamp",
-                "type": "string",
-                "format": "date-time",
-                "array": False,
-                'req': False,
-                'hierarchy': ['timestamp']
-            },
-            "bundle_uuid": {
-                "description": "A RFC4122-compliant ID.",
-                "in": "body",
-                "name": "bundle_uuid",
-                "type": "string",
-                "array": False,
-                'req': True,
-                'hierarchy': ['bundle_uuid']
-            },
-            'top_level_body_params': {
-                "timestamp": {
-                    "description": "Timestamp of file creation in RFC3339.",
-                    "in": "body",
-                    "name": "timestamp",
-                    "type": "string",
-                    "format": "date-time",
-                    "array": False,
-                    'req': False,
-                    'hierarchy': ['timestamp']
-                },
-                "bundle_uuid": {
-                    "description": "A RFC4122-compliant ID.",
-                    "in": "body",
-                    "name": "bundle_uuid",
-                    "type": "string",
-                    "array": False,
-                    'req': True,
-                    'hierarchy': ['bundle_uuid']
-            }}}
-        )
-
     def test_get_files_cli(self):
-        """Testing 2 things: 1. Printing bytes to stdout; 2. Iterative content download."""
+        filename = "SRR2967608_1.fastq.gz"
+        dirpath = os.path.join(os.path.dirname(os.path.realpath(__file__)), "bundle")
+        file_path = os.path.join(dirpath, filename)
 
-        dirpath = os.path.dirname(os.path.realpath(__file__))
-        file_path = os.path.join(dirpath, "bundle", "SRR2967608_1.fastq.gz")
+        replica = "aws"
+        staging_bucket = "org-humancellatlas-dss-cli-test"
+        client = hca.dss.DSSClient()
+        response = client.upload(src_dir=dirpath, replica=replica, staging_bucket=staging_bucket)
 
-        self.assertGreater(os.stat(file_path).st_size, hca.dss.constants.Constants.CHUNK_SIZE)
+        for f in response["files"]:
+            print(f)
+            if f["name"] == filename:
+                file_uuid = f['uuid']
+                break
 
-        args = {'file_or_dir': [file_path],
-                'replica': "aws",
-                'staging_bucket': "org-humancellatlas-dss-cli-test"}
-        response = hca.dss.upload(**args)
+        res = client.get_file(uuid=file_uuid, replica=replica)
 
-        file_uuid = response['files'][0]['uuid']
-
-        process = subprocess.Popen(["hca", "dss", "get-files", file_uuid, "--replica", "aws"], stdout=subprocess.PIPE)
-        stdout, stderr = process.communicate()
-        self.assertIsInstance(stdout, bytes)
+        self.assertIsInstance(res, bytes)
         with open(file_path, "rb") as bytes_fh:
             file_content = bytes_fh.read()
-            if file_content != stdout:
-                self.fail("output does not match file content: \"%s\"" % stdout)
-
-    def _parse_args(self, command):
-        import argparse
-        command_line_args = command if isinstance(command, list) else command.split(" ")
-        parser = argparse.ArgumentParser()
-        subparsers = parser.add_subparsers()
-        hca.dss.cli.add_commands(subparsers)
-        argparser_args = parser.parse_args(command_line_args)
-        func = argparser_args.func
-        del argparser_args.func
-        api_args = hca.dss.cli.parse_args(argparser_args)
-        return func, api_args
-
-    def test_arg_parsing(self):
-        """Test that the parser parses arguments correctly."""
-        args = ["dss", "put-files", "134", "--bundle-uuid", "asdf", "--creator-uid", "1", "--source-url", "sljdf.com"]
-        expected = {'source_url': 'sljdf.com', 'bundle_uuid': 'asdf', 'uuid': '134', 'creator_uid': 1}
-        func, api_args = self._parse_args(args)
-        self.assertEqual(func, hca.dss.autogenerated.put_files.PutFiles.run_from_cli)
-        self.assertEqual(api_args, expected)
-
-        args = ["dss", "put-files", "--bundle-uuid", "asdf", "--creator-uid", "1", "--source-url", "sljdf.com", "134"]
-        expected = {'source_url': 'sljdf.com', 'bundle_uuid': 'asdf', 'uuid': '134', 'creator_uid': 1}
-        func, api_args = self._parse_args(args)
-        self.assertEqual(func, hca.dss.autogenerated.put_files.PutFiles.run_from_cli)
-        self.assertEqual(api_args, expected)
-
-        args = ["dss", "put-files", "--replica", "aws", "--creator-uid", "1", "--source-url", "sljdf.com", "134"]
-        process = subprocess.Popen(["hca"] + args, stdout=subprocess.PIPE, stderr=subprocess.PIPE)
-        stdout, stderr = process.communicate()
-        expected_error = '--bundle-uuid is required' if six.PY2 else 'are required: --bundle-uuid'
-        six.assertRegex(self, stderr.decode('utf8'), expected_error)
-
-        args = ["dss", "put-files", "--bundle-uuid", "asdf", "--creator-uid", "1", "--source-url", "sljdf.com"]
-        process = subprocess.Popen(["hca"] + args, stdout=subprocess.PIPE, stderr=subprocess.PIPE)
-        stdout, stderr = process.communicate()
-        expected_error = 'too few arguments' if six.PY2 else 'are required: uuid'
-        six.assertRegex(self, stderr.decode('utf8'), expected_error)
-
-        args = ["dss", "put-files", "--bundle-uuid", "--creator-uid", "1", "--source-url", "sljdf.com", "134"]
-        process = subprocess.Popen(["hca"] + args, stdout=subprocess.PIPE, stderr=subprocess.PIPE)
-        stdout, stderr = process.communicate()
-        six.assertRegex(self, stderr.decode('utf8'), '--bundle-uuid: expected one argument')
-
-        args = ["dss", "get-bundles", "--replica", "aws", "uuid_arg"]
-        expected = {"uuid": "uuid_arg", "replica": "aws"}
-        func, api_args = self._parse_args(args)
-        self.assertEqual(func, hca.dss.autogenerated.get_bundles.GetBundles.run_from_cli)
-        self.assertEqual(api_args, expected)
-
-        args = ["dss", "get-bundles", "uuid_arg", "--version", "version_arg", "--replica", "rep"]
-        expected = {"uuid": "uuid_arg", "replica": "rep", "version": "version_arg"}
-        func, api_args = self._parse_args(args)
-        self.assertEqual(func, hca.dss.autogenerated.get_bundles.GetBundles.run_from_cli)
-        self.assertEqual(api_args, expected)
-
-        # Works for now but shouldn't in the future b/c --replica required when uuid and version specified.
-        args = ["dss", "get-bundles", "--replica", "aws", "uuid_arg", "--version", "version_arg"]
-        expected = {"uuid": "uuid_arg", "version": "version_arg", "replica": "aws"}
-        func, api_args = self._parse_args(args)
-        self.assertEqual(func, hca.dss.autogenerated.get_bundles.GetBundles.run_from_cli)
-        self.assertEqual(api_args, expected)
-
-        # Works for now. --replica isn't an option unless both uuid and version specified.
-        args = ["dss", "get-bundles", "uuid_arg", "--replica", "rep"]
-        expected = {"uuid": "uuid_arg", "replica": "rep"}
-        func, api_args = self._parse_args(args)
-        self.assertEqual(func, hca.dss.autogenerated.get_bundles.GetBundles.run_from_cli)
-        self.assertEqual(api_args, expected)
+            self.assertEqual(file_content, res)
 
     @reset_tweak_changes
     def test_cli_login(self):
-        from tweak import Config
-
-        cli = CLI()
-
+        """Test that the login command works with a dummy token"""
         access_token = "test_access_token"
-        expected = '{"completed": true}\n'
+        expected = "Storing access credentials\n"
         args = ["dss", "login", "--access-token", access_token]
 
         with CapturingIO('stdout') as stdout:
-            cli.run(args)
-            config = Config(hca.TWEAK_PROJECT_NAME)
+            hca.cli.main(args)
 
         self.assertEqual(stdout.captured(), expected)
-        self.assertEqual(config.login.access_token, access_token)
-
-    def test_array_cli(self):
-        """Ensure that this framework can handle arrays."""
-
-        args = ["dss", "put-bundles", "uuid", "--version", "version", "--files", "uuid1/v1/n1/True", "uuid2/v2/n2/False", "--creator-uid", "3", "--replica", "rep"]
-        expected = {
-            "uuid": "uuid", "version": "version",
-            "files": ["uuid1/v1/n1/True", "uuid2/v2/n2/False"], "creator_uid": 3, "replica": "rep"
-        }
-        func, api_args = self._parse_args(args)
-        self.assertEqual(func, hca.dss.autogenerated.put_bundles.PutBundles.run_from_cli)
-        self.assertEqual(api_args, expected)
-
-    def test_parsing_array_object_literals(self):
-        """Make sure that parsing literals works within an array object."""
-        from hca.dss.autogenerated.put_bundles import PutBundles  # noqa
-
-        args = ["dss", "put-bundles", "234sf", "--files", "True/n1/u1/v1", "False/n2/u2/v2", "--replica" ,"rep", "--creator-uid", "8"]
-        func, api_args = self._parse_args(args)
-        out = {'files': [{'indexed': True, 'version': 'v1', 'uuid': 'u1', 'name': 'n1'}, {'indexed': False, 'version': 'v2', 'uuid': 'u2', 'name': 'n2'}], 'creator_uid': 8}
-        body_payload = PutBundles._build_body_payload(api_args)
-        self.assertEqual(body_payload, out)
+        self.assertEqual(hca.get_config().oauth2_token.access_token, access_token)
 
     def test_json_input(self):
-        """Ensure that adding json input works."""
-        args = ["dss", "post-search", "--es-query", '{"hello":"world", "goodbye":"earth"}', "--replica", "aws"]
-        func, api_args = self._parse_args(args)
-        out = {"es_query": {"hello": "world", "goodbye": "earth"}, "replica": "aws"}
-        self.assertEqual(out, api_args)
+        """Ensure that adding JSON input works"""
+        args = ["dss", "post-search", "--es-query", '{}', "--replica", "aws"]
+        with CapturingIO('stdout') as stdout:
+            hca.cli.main(args)
 
-    @unittest.skip("The bundle referenced in this test is no longer in place. Plans to set up an upload bundle")
-    def test_upload_to_cloud_from_s3(self):
-        import hca.dss.upload_to_cloud
-        uuids, names = hca.dss.upload_to_cloud.upload_to_cloud(
-            ["s3://hca-dss-test-src/data-bundles-examples/import/10x/pbmc8k/bundles/bundle1/"],
-            "pointless-staging-bucket",
-            "aws",
-            True
-        )
-        out = [
-            "data-bundles-examples/import/10x/pbmc8k/bundles/bundle1/assay.json",
-            "data-bundles-examples/import/10x/pbmc8k/bundles/bundle1/project.json",
-            "data-bundles-examples/import/10x/pbmc8k/bundles/bundle1/sample.json"
-        ]
-        self.assertEqual(len(uuids), len(names))
-        assert_list_items_equal = (self.assertCountEqual if six.PY3
-                                   else self.assertItemsEqual)
-        assert_list_items_equal(names, out)
+        self.assertEqual(json.loads(stdout.captured())["es_query"], {})
 
 
 if __name__ == '__main__':
