6e4c6ea56171ceebfcb27035a2736d555017552b
Andrey Kislyuk <kislyuk@gmail.com>
2018-01-21 21:27:48 -0800

    Avoid crashing when writing of config file fails

--- hca/util/compat.py
+++ hca/util/compat.py
@@ -3,3 +3,10 @@ from __future__ import absolute_import, division, print_function, unicode_litera
 import os, sys, datetime, errno
 
 USING_PYTHON2 = True if sys.version_info < (3, 0) else False
+
+if USING_PYTHON2:
+    from pathlib2 import Path
+    from funcsigs import signature, Signature, Parameter
+else:
+    from pathlib import Path
+    from inspect import signature, Signature, Parameter
