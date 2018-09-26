e0f71ec28cadb80ee35e6e2f26813290544c1e15
Trent Smith <tsmith12@ucsc.edu>
2018-09-26 11:52:55 -0700

    v4.4.2

--- setup.py
+++ setup.py
@@ -7,7 +7,7 @@ install_requires = [line.rstrip() for line in open(os.path.join(os.path.dirname(
 
 setup(
     name="hca",
-    version="4.4.1",
+    version="4.4.2",
     url='https://github.com/HumanCellAtlas/dcp-cli',
     license='Apache Software License',
     author='Human Cell Atlas contributors',
