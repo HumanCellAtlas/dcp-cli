da130f4ea8958d82678ba6ba14ed5d017fde2e18
Trent Smith <tsmith12@ucsc.edu>
2018-09-28 14:24:34 -0700

    Remove redundant index on landing page

--- docs/conf.py
+++ docs/conf.py
@@ -95,7 +95,10 @@ todo_include_todos = True
 # further.  For a list of options available for each theme, see the
 # documentation.
 #
-# html_theme_options = {}
+html_theme_options = {
+    'sidebar_includehidden': True,
+    'sidebar_collapse': True
+}
 
 # Add any paths that contain custom static files (such as style sheets) here,
 # relative to this directory. They are copied after the builtin static files,
