c264cc3af4df5b1c209eed0e7f433835c260195a
amar jandu <ajandu@ucsc.edu>
2019-03-05 09:48:56 -0800

    modified exception

--- hca/dss/error.py
+++ hca/dss/error.py
@@ -1,10 +1,10 @@
 from ..util.exceptions import SwaggerAPIException
 from .. import logger
 
+
 class APIException(SwaggerAPIException):
     def __init__(self, *args, **kwargs):
-        print(self.response.headers)
-        if 'response' in kwargs:
-            if kwargs['response']['headers']['AWS-Request-ID']:
-                logger.error("%s", "X-AWS-REQUEST-ID: {}".format(kwargs['response']['headers']["X-AWS-REQUEST-ID"]))
         super(APIException, self).__init__(*args, **kwargs)
+        if self.response:
+            if self.response['headers']['AWS-Request-ID']:
+                logger.error("%s", "X-AWS-REQUEST-ID: {}".format(self.response['headers']['AWS-Request-ID']))
