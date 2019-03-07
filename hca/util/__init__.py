aef6973e0a07b62afbe3263cac5337eb6f4444ce
DailyDreaming <lblauvel@ucsc.edu>
2019-03-06 16:26:11 -0800

    Get bundle support.

--- hca/util/__init__.py
+++ hca/util/__init__.py
@@ -180,11 +180,10 @@ class _PaginatingClientMethodFactory(_ClientMethodFactory):
         page = None
         while page is None or page.links.get("next", {}).get("url"):
             page = self._request(kwargs, url=page.links["next"]["url"] if page else None)
-            if "results" in page:
+            try:
                 for result in page.json()["results"]:
                     yield result
-            # assume this is a get bundle response
-            else:
+            except KeyError:
                 for file in page.json()["bundle"]["files"]:
                     yield file
 
