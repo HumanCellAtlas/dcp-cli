3ef913da92eb225ac8008919e605b87f8f998312
amar jandu <ajandu@ucsc.edu>
2019-05-16 13:33:46 -0700

    looped + tested

--- hca/util/__init__.py
+++ hca/util/__init__.py
@@ -381,11 +381,11 @@ class SwaggerClient(object):
         """
         Clear {prog} authentication credentials previously configured with ``{prog} login``.
         """
-        try:
-            del self.config["application_secrets"]
-            del self.config["oauth2_token"]
-        except KeyError:
-            pass
+        for keys in ["application_secrets","oauth2_token"]:
+            try:
+                del self.config[keys]
+            except KeyError:
+                pass
 
     def login(self, access_token="", remote=False):
         """
