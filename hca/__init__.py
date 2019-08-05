aed57a47066a502f519177e478d0ece98d6e689d
Andrey Kislyuk <kislyuk@gmail.com>
2019-08-05 16:14:42 -0700

    Autodocs: call all SwaggerClient constructors (#416)

--- hca/__init__.py
+++ hca/__init__.py
@@ -1,4 +1,4 @@
 from __future__ import absolute_import, division, print_function, unicode_literals
 
 from .config import HCAConfig, get_config, logger
-from . import dss, upload
+from . import dss, upload, query
