18cbdbf28646826eb8b95c0197b9a91941bdaae2
DailyDreaming <lblauvel@ucsc.edu>
2019-09-03 17:28:10 -0700

    One last stab.

--- hca/query/cli.py
+++ hca/query/cli.py
@@ -1,7 +1,3 @@
-from __future__ import absolute_import, division, print_function, unicode_literals
-
-import sys
-
 from . import DCPQueryClient
 
 def add_commands(subparsers, help_menu=False):
@@ -10,8 +6,7 @@ def add_commands(subparsers, help_menu=False):
     def help(args):
         query_parser.print_help()
 
-    if sys.version_info >= (2, 7, 9):  # See https://bugs.python.org/issue9351
-        query_parser.set_defaults(entry_point=help)
+    query_parser.set_defaults(entry_point=help)
     query_subparsers = query_parser.add_subparsers()
     query_cli_client = DCPQueryClient()
     query_cli_client.build_argparse_subparsers(query_subparsers, help_menu=help_menu)
