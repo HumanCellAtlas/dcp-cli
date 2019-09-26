35b7c58f1b31d8885f67cc34f274c0a174aba8de
Charles Reid <53452777+chmreid@users.noreply.github.com>
2019-09-26 12:26:03 -0700

    Fixes for Sphinx API demo documents (#442)
    * formatting fixes for API sample scripts
    
    * rename missing .rst file
    
    * formatting and language fixes for OpenAPI examples doc

--- test/tutorial/scripts/api/get_bundles_checkout_api.py
+++ test/tutorial/scripts/api/get_bundles_checkout_api.py
@@ -2,9 +2,10 @@ from hca.dss import DSSClient
 
 dss = DSSClient()
 
-bundle_checkout_status = dss.get_bundles_checkout(replica="aws",
-                                                  checkout_job_id='4de1c603-fa8b-4c07-af37-06159e6951e0')
+bundle_checkout_status = dss.get_bundles_checkout(
+    replica="aws", checkout_job_id="4de1c603-fa8b-4c07-af37-06159e6951e0"
+)
 
 print(f'Bundle checkout status: {bundle_checkout_status["status"]}!')
-if bundle_checkout_status["status"] == 'SUCCEEDED':
+if bundle_checkout_status["status"] == "SUCCEEDED":
     print(f'File is located at: {bundle_checkout_status["location"]}')
