0e6b75dddeb85ab16e1928a02a0b958f39898594
Trent Smith <trent.smith@chanzuckerberg.com>
2019-09-26 11:21:41 -0700

    fixing tests

--- test/integration/auth/test_auth_api.py
+++ test/integration/auth/test_auth_api.py
@@ -8,17 +8,9 @@ pkg_root = os.path.abspath(os.path.join(os.path.dirname(__file__), '..', '..', '
 sys.path.insert(0, pkg_root)  # noqa
 
 import hca.auth
-from hca.util.compat import USING_PYTHON2
+import unittest
 
-if USING_PYTHON2:
-    import backports.tempfile
-    import unittest2 as unittest
-
-    TemporaryDirectory = backports.tempfile.TemporaryDirectory
-else:
-    import unittest
-
-    TemporaryDirectory = tempfile.TemporaryDirectory
+TemporaryDirectory = tempfile.TemporaryDirectory
 
 
 class TestDssApi(unittest.TestCase):
@@ -28,7 +20,7 @@ class TestDssApi(unittest.TestCase):
         cls.client = hca.auth.AuthClient(swagger_url="https://auth.dev.data.humancellatlas.org/swagger.json")
 
     def test_smoke(self):
-        self.client.get_well_known_openid_configuration()
+        self.client.get_well_known_openid_configuration(host="auth.dev.data.humancellatlas.org")
 
 
 if __name__ == "__main__":
