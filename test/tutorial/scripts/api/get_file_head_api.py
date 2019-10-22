4eb4cd7763bfed9d4368b918e99eb59c1cf98a8a
Charles Reid <chmreid@ucsc.edu>
2019-10-22 15:31:57 -0400

    extract headers in get_file_head_api script

--- test/tutorial/scripts/api/get_file_head_api.py
+++ test/tutorial/scripts/api/get_file_head_api.py
@@ -2,18 +2,24 @@ from hca.dss import DSSClient
 
 dss = DSSClient()
 
-print(
-    dss.head_file(
-        uuid="6887bd52-8bea-47d9-bbd9-ff71e05faeee",
-        replica="aws"
-    )
+print("Calling dss.head_file() with a file UUID:")
+response = dss.head_file(
+    uuid="6887bd52-8bea-47d9-bbd9-ff71e05faeee",
+    replica="aws",
 )
+if response.status_code==200:
+    print("Success!")
+    print("Headers: %s"%(response.headers))
 
-# Can add optional version
-print(
-    dss.head_file(
-        uuid="6887bd52-8bea-47d9-bbd9-ff71e05faeee",
-        replica="aws",
-        version="2019-01-30T165057.189000Z",
-    )
+print()
+
+# Optionally, add a version
+print("Calling dss.head_file() with a file UUID and version:")
+response = dss.head_file(
+    uuid="6887bd52-8bea-47d9-bbd9-ff71e05faeee",
+    replica="aws",
+    version="2019-01-30T165057.189000Z",
 )
+if response.status_code==200:
+    print("Success!")
+    print("Headers: %s"%(response.headers))
