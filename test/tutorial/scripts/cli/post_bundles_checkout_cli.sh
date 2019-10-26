092f49650adc7291138b40c237fcedefdfffa3fd
Charles Reid <chmreid@ucsc.edu>
2019-10-26 12:19:22 -0700

    cli scripts - more update/clean/make executable

--- test/tutorial/scripts/cli/post_bundles_checkout_cli.sh
+++ test/tutorial/scripts/cli/post_bundles_checkout_cli.sh
@@ -1,3 +1,5 @@
 #!/usr/bin/env bash
 
-hca dss post-bundles-checkout --uuid fff746b3-e3eb-496a-88a3-5fa1fa358392 --replica aws
+hca dss post-bundles-checkout \
+    --replica aws \
+    --uuid fff746b3-e3eb-496a-88a3-5fa1fa358392
--- test/tutorial/scripts/cli/upload_cli.sh
+++ test/tutorial/scripts/cli/upload_cli.sh
@@ -1,5 +1,8 @@
 #!/usr/bin/env bash
 
-hca dss upload --src-dir data/ --replica aws --staging-bucket upload-test-unittest
+hca dss upload \
+    --src-dir data/ \
+    --replica aws \
+    --staging-bucket upload-test-unittest
 
 aws s3 rm s3://upload-test-unittest --recursive
