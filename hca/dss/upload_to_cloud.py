29286c2e691d5cc0a95a7f4f73b3076e1ba3faee
Geryl Pelayo <gpelayo1@ucsc.edu>
2020-02-10 10:05:03 -0800

    File handlers close after use

--- hca/dss/upload_to_cloud.py
+++ hca/dss/upload_to_cloud.py
@@ -53,12 +53,12 @@ def _copy_from_s3(path, s3):
     return file_uuids, key_names
 
 
-def upload_to_cloud(file_handles, staging_bucket, replica, from_cloud=False, log_progress=False):
+def upload_to_cloud(file_paths, staging_bucket, replica, from_cloud=False, log_progress=False):
     """
     Upload files to cloud.
 
-    :param file_handles: If from_cloud, file_handles is a aws s3 directory path to files with appropriate
-                         metadata uploaded. Else, a list of binary file_handles to upload.
+    :param file_paths: If from_cloud, file_handles is a aws s3 directory path to files with appropriate
+                       metadata uploaded. Else, a list of paths of files to upload.
     :param staging_bucket: The aws bucket to upload the files to.
     :param replica: The cloud replica to write to. One of 'aws', 'gc', or 'azure'. No functionality now.
     :param bool log_progress: set to True to log progress to stdout. Progress bar will reflect bytes
@@ -76,44 +76,45 @@ def upload_to_cloud(file_handles, staging_bucket, replica, from_cloud=False, log
     log_progress = all((logger.getEffectiveLevel() <= logging.INFO, sys.stdout.isatty(), log_progress))
 
     if from_cloud:
-        file_uuids, key_names = _copy_from_s3(file_handles[0], s3)
+        file_uuids, key_names = _copy_from_s3(file_paths[0], s3)
     else:
         destination_bucket = s3.Bucket(staging_bucket)
         if log_progress:
-            total_upload_size = sum(os.fstat(f.fileno()).st_size for f in file_handles)
+            total_upload_size = sum(os.path.getsize(f) for f in file_paths)
             logger.addHandler(ProgressBarStreamHandler())
             progress = tqdm.tqdm(total=total_upload_size, desc="Uploading to " + replica,
                                  unit="B", unit_scale=True, unit_divisor=1024)
-        for raw_fh in file_handles:
-            file_size = os.path.getsize(raw_fh.name)
-            multipart_chunksize = s3_multipart.get_s3_multipart_chunk_size(file_size)
-            tx_cfg = TransferConfig(multipart_threshold=s3_multipart.MULTIPART_THRESHOLD,
-                                    multipart_chunksize=multipart_chunksize)
-            with ChecksummingBufferedReader(raw_fh, multipart_chunksize) as fh:
-                file_uuid = str(uuid.uuid4())
-                key_name = "{}/{}".format(file_uuid, os.path.basename(fh.raw.name))
-                destination_bucket.upload_fileobj(
-                    fh,
-                    key_name,
-                    Config=tx_cfg,
-                    Callback=lambda x: progress.update(x) if log_progress else None,
-                    ExtraArgs={
-                        'ContentType': _mime_type(fh.raw.name),
+        for file_path in file_paths:
+            with open(file_path, 'rb') as raw_fh:
+                file_size = os.path.getsize(raw_fh.name)
+                multipart_chunksize = s3_multipart.get_s3_multipart_chunk_size(file_size)
+                tx_cfg = TransferConfig(multipart_threshold=s3_multipart.MULTIPART_THRESHOLD,
+                                        multipart_chunksize=multipart_chunksize)
+                with ChecksummingBufferedReader(raw_fh, multipart_chunksize) as fh:
+                    file_uuid = str(uuid.uuid4())
+                    key_name = "{}/{}".format(file_uuid, os.path.basename(fh.raw.name))
+                    destination_bucket.upload_fileobj(
+                        fh,
+                        key_name,
+                        Config=tx_cfg,
+                        Callback=lambda x: progress.update(x) if log_progress else None,
+                        ExtraArgs={
+                            'ContentType': _mime_type(fh.raw.name),
+                        }
+                    )
+                    sums = fh.get_checksums()
+                    metadata = {
+                        "hca-dss-s3_etag": sums["s3_etag"],
+                        "hca-dss-sha1": sums["sha1"],
+                        "hca-dss-sha256": sums["sha256"],
+                        "hca-dss-crc32c": sums["crc32c"],
                     }
-                )
-                sums = fh.get_checksums()
-                metadata = {
-                    "hca-dss-s3_etag": sums["s3_etag"],
-                    "hca-dss-sha1": sums["sha1"],
-                    "hca-dss-sha256": sums["sha256"],
-                    "hca-dss-crc32c": sums["crc32c"],
-                }
-                s3.meta.client.put_object_tagging(Bucket=destination_bucket.name,
-                                                  Key=key_name,
-                                                  Tagging=dict(TagSet=encode_tags(metadata)))
-                file_uuids.append(file_uuid)
-                key_names.append(key_name)
-                abs_file_paths.append(fh.raw.name)
+                    s3.meta.client.put_object_tagging(Bucket=destination_bucket.name,
+                                                      Key=key_name,
+                                                      Tagging=dict(TagSet=encode_tags(metadata)))
+                    file_uuids.append(file_uuid)
+                    key_names.append(key_name)
+                    abs_file_paths.append(fh.raw.name)
         if log_progress:
             logger.handlers = [l for l in logger.handlers if not isinstance(l, ProgressBarStreamHandler)]
             progress.close()
