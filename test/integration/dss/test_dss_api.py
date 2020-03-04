61cb5759f5f3016b05e265f2cebb0b298a18805e
Geryl Pelayo <gpelayo1@ucsc.edu>
2020-03-03 16:53:29 -0800

    testing put bundle and files

--- test/integration/dss/test_dss_api.py
+++ test/integration/dss/test_dss_api.py
@@ -507,12 +507,12 @@ class TestProdDSSApi(unittest.TestCase):
         self.file_uuid = 'dd78d5eb-7cb0-4112-81fe-90e1aecb81db'
         self.file_version = '2019-10-01T200147.402537Z'
         source_url = "s3://{}/{}/{}".format('org-humancellatlas-dss-cli-test', '00109752-d6af-4c93-a20c-82373eb5c838', self.test_prod_file)
-        resp = self.client.put_file(uuid='dd78d5eb-7cb0-4112-81fe-90e1aecb81db',
-                                    creator_uid=1,
-                                    bundle_uuid='00004ffa-a7e7-4848-9cb8-9fdbb3b2549f',
-                                    version='2019-10-01T200147.402537Z',
-                                    source_url=source_url)
-        logger.info(json.dumps(resp, indent=4))
+        # resp = self.client.put_file(uuid='dd78d5eb-7cb0-4112-81fe-90e1aecb81db',
+        #                             creator_uid=1,
+        #                             bundle_uuid='00004ffa-a7e7-4848-9cb8-9fdbb3b2549f',
+        #                             version='2019-10-01T200147.402537Z',
+        #                             source_url=source_url)
+        # logger.info(json.dumps(resp, indent=4))
         test_file = [{'indexed': True,
                       'name': self.test_prod_file,
                       'uuid': self.file_uuid,
