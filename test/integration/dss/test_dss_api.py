b5fbe3a26b671238448ffb56d61ae4aa138d0106
Geryl Pelayo <gpelayo1@ucsc.edu>
2020-03-10 13:38:38 -0700

    testing put_bundle and put_file - changed file content + version

--- test/integration/dss/test_dss_api.py
+++ test/integration/dss/test_dss_api.py
@@ -505,13 +505,13 @@ class TestProdDSSApi(unittest.TestCase):
     def test_put_bundle(self):
         self.test_prod_file = 'SRR2967608_1.fastq.gz'
         self.file_uuid = 'dd78d5eb-7cb0-4112-81fe-90e1aecb81db'
-        self.file_version = '2020-03-10T200147.402537Z'
+        self.file_version = '2020-03-09T200147.402537Z'
         source_url = "s3://{}/{}/{}".format('org-humancellatlas-dss-cli-test', '00109752-d6af-4c93-a20c-82373eb5c838',
                                             self.test_prod_file)
-        resp = self.client.put_file(uuid='dd78d5eb-7cb0-4112-81fe-90e1aecb81db',
+        resp = self.client.put_file(uuid='dd78d5åeb-7cb0-4112-81fe-90e1aecb81db',
                                     creator_uid=1,
                                     bundle_uuid='00004ffa-a7e7-4848-9cb8-9fdbb3b2549f',
-                                    version='2020-03-10T200147.402537Z',
+                                    version='2020-03-09T200147.402537Z',
                                     source_url=source_url)
         logger.info(json.dumps(resp, indent=4))
         test_file = [{'indexed': True,
